c6c3165c40e93ede4e8c5c3ef45af7afa9396556
Linus Stetter <linus.stetter@px5.de>
2025-06-23 23:07:42 +0200

    chore: update vite.config.js and package.json to include mongoose dependency

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,17 +6,30 @@
 //   plugins: [react()],
 // })
 
-import path from "path";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+// import path from "path";
+// import tailwindcss from "@tailwindcss/vite";
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
 });
