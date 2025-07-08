d6f0c92248acfc53cac2fb89f2e4aeb4b181da1a
Jasmin Thio <jasminthio@gmail.com>
2025-07-08 18:42:06 +0200

    HelperSession Daten ausgelagert

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -18,21 +18,8 @@ import duggyLogo from "/src/assets/duggy-logo.png";
 
 import Navbar from "@/components/Navbar";
 
-class HelperSession {
-  constructor(initialData) {
-    this.initialEditorContent = initialData.initialEditorContent;
-    this.textHint = initialData.textHint;
-    this.codeHint = initialData.codeHint;
-    this.solution = initialData.solution;
-    this.problemDescription = initialData.problemDescription;
-    this.keyConcepts = initialData.keyConcepts || [];
-  }
 
-  updateHints(textHint, codeHint) {
-    this.textHint = textHint;
-    this.codeHint = codeHint;
-  }
-}
+import HelperSession from "@/lib/HelperSession";
 
 export default function HomePage() {
   const [language, setLanguage] = useState("python");
